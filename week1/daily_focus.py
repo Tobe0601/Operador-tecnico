42d824cdbff204076d3e139aaf7c5f6db493daf3
Luis Morales <luismoralesnk@gmail.com>
2026-03-02 09:37:38 -0800

    Day 1 - Initial project structure and first script

---
+++ week1/daily_focus.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+hours = float(input("How many hours did you study today? "))
+
+if hours >= 1:
+    print(f"Excelent {name}. Operator in construction.")
+    
+else:
+    print(f"{name}, discipline before motivation.")    

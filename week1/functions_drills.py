b8ea324988ffd96d0d0eff6b7a9d69bbed90e083
Luis Morales <luismoralesnk@gmail.com>
2026-03-12 09:05:24 -0700

    Add function exercises

--- week1/functions_drills.py
+++ week1/functions_drills.py
@@ -7,3 +7,17 @@ def hours_to_minutes (hours):
     return hours * 60
 
 print(hours_to_minutes(5))
+
+def is_between(x, low, high):
+    return low <= x <= high
+
+print(is_between(5, 1, 10))
+print(is_between(15, 1, 10))
+
+
+import math
+
+def circle_area(radius):
+    return math.pi * radius ** 2
+
+print(circle_area(3))

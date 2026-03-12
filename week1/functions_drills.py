81f218bc3878a11f7ec696255874dfd2884894da
Luis Morales <luismoralesnk@gmail.com>
2026-03-12 09:37:20 -0700

    Adding comments and multiplication exercise

--- week1/functions_drills.py
+++ week1/functions_drills.py
@@ -1,23 +1,34 @@
+#Suma basica
 def add(a, b):
     return a + b
 
 print(add(2, 3))
 
+#Conversor de horas a minutos
 def hours_to_minutes (hours):
     return hours * 60
 
 print(hours_to_minutes(5))
 
+#Identificar si un numero esta en un rango entre dos numeros mas
 def is_between(x, low, high):
     return low <= x <= high
 
 print(is_between(5, 1, 10))
 print(is_between(15, 1, 10))
 
-
+#Calculando el area de un circulo
 import math
 
 def circle_area(radius):
     return math.pi * radius ** 2
 
 print(circle_area(3))
+
+
+#Multiplicando
+def multiply(a, b):
+    return a * b
+
+print(multiply(4, 6))
+print(multiply(10, 3))

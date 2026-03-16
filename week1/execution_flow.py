d1b2ed3364e9ecd98b7848022e8c1498bea943ff
Luis Morales <luismoralesnk@gmail.com>
2026-03-16 09:04:45 -0700

    Add income calculator exercise

--- week1/execution_flow.py
+++ week1/execution_flow.py
@@ -8,3 +8,14 @@ print("3")
 test()
 
 print("4")
+
+
+def weekly_pay(hours_per_day,rate):
+    return hours_per_day * rate * 7
+
+hours = float(input("How many hours do you work per day?: "))
+rate = float(input("How much do you get per hour?: "))
+
+result = weekly_pay(hours, rate)
+
+print(result)

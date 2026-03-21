fbb3ae91a80e56acd6ca62591a10188f4584bd77
Luis Morales <luismoralesnk@gmail.com>
2026-03-21 09:01:13 -0700

    add tuple exercise

--- week1/tuples.py
+++ week1/tuples.py
@@ -1,1 +1,10 @@
-tupla = ("dato 1", "dato 2")
+#Creando tuplas con tuple()
+tupla = tuple(["dato 1", "dato 2"])
+
+#Creando tupla de multiples datos sin parentesis.
+tupla = "dato1", "dato2"
+
+#creando tupla de un solo dato sin parentesis.
+tupla = "dato1", #<-- Solo agregamos una coma al final
+ 
+print (type(tupla))
